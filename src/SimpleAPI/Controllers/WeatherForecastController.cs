f46ff6502285383b2ebac9247336b5de6b9556e3
aglaude69 <aglaude69@gmail.com>
2021-04-28 16:11:43 -0400

    Fix WF control for the Test error

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -40,7 +40,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "@WeatherForecast " + id.ToString() ;
+            return "WeatherForecast " + id.ToString() ;
         }
     }
 }
